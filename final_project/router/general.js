1abab133c0959cba3441df9535a3e3c45a878b19
x-student-x <148026385+x-student-x@users.noreply.github.com>
2024-01-05 15:37:00 -0600

    login

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -33,6 +33,25 @@ public_users.post("/register", (req,res) => {
 
 });
 
+public_users.post("/customer/login", (req,res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    if (!username || !password) {
+        return res.status(404).json({message: "Error logging in"});
+    }
+   if (authenticatedUser(username,password)) {
+      let accessToken = jwt.sign({
+        data: password
+      }, 'access', { expiresIn: 60 * 60 });
+      req.session.authorization = {
+        accessToken,username
+    }
+    return res.status(200).send("User successfully logged in");
+    } else {
+      return res.status(208).json({message: "Invalid Login. Check username and password"});
+    }});
+
+
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
@@ -47,7 +66,7 @@ public_users.get('/isbn/:isbn',function (req, res) {
   const bookByISBN = req.params.isbn;
   return res.send(books[bookByISBN])
  });
-  
+
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
