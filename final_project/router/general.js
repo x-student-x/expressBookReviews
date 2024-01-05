b8f16c3caebbe17e60f354e45c2ea464e39681a3
x-student-x <148026385+x-student-x@users.noreply.github.com>
2024-01-05 13:28:14 -0600

    update

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -21,39 +21,71 @@ public_users.get('/',function (req, res) {
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
   //return res.status(300).json({message: "Yet to be implemented"});
-  const isbn = req.params.isbn;
-  return res.send(books[isbn])
+  const bookByISBN = req.params.isbn;
+  return res.send(books[bookByISBN])
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
   //return res.status(300).json({message: "Yet to be implemented"});
-  const auth = req.params.author;
-  const bookList = [];
-  Object.entries(books).forEach(([author, details]) => {
-    //console.log(`${JSON.stringify(author)}: ${JSON.stringify(details)}`);
+  const byAuthor = req.params.author;
+  const bookByAuthor = [];
+  Object.entries(books).forEach(([id, details]) => {
+    //console.log(`${JSON.stringify(id)}: ${JSON.stringify(details)}`);
     Object.entries(details).forEach(([keys, values]) =>{
-        console.log(`${JSON.stringify(keys)}: ${JSON.stringify(values)}`)
-        if(JSON.stringify(values) === JSON.stringify(auth)){
-            console.log("found?")
-            bookList.push(author,details)
+        //console.log(`${JSON.stringify(keys)}: ${JSON.stringify(values)}`)
+        if(JSON.stringify(values) === JSON.stringify(byAuthor)){
+            bookByAuthor.push(id,details)
         }
     });
   });
-  res.send(bookList)
+  return res.send(bookByAuthor)
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  //return res.status(300).json({message: "Yet to be implemented"});
+  const byTitle = req.params.title;
+  const bookBytitle = [];
+  Object.entries(books).forEach(([id, details]) => {
+    //console.log(`${JSON.stringify(id)}: ${JSON.stringify(details)}`);
+    Object.entries(details).forEach(([keys, values]) =>{
+        //console.log(`${JSON.stringify(keys)}: ${JSON.stringify(values)}`)
+        if(JSON.stringify(values) === JSON.stringify(byTitle)){
+            bookBytitle.push(id,details)
+        }
+    });
+  });
+  return res.send(bookBytitle)
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  //return res.status(300).json({message: "Yet to be implemented"});
+  const reviewByISBN = req.params.isbn;
+  var showISBNTitle = ""
+  const reviewsForISBN = [];
+  //return res.send(books[reviewByISBN])
+  Object.entries(books).forEach(([id, details]) => {
+    //console.log(`${JSON.stringify(id)}: ${JSON.stringify(details)}`);
+    Object.entries(details).forEach(([keys, values]) => {
+      //console.log(`${JSON.stringify(keys)}: ${JSON.stringify(values)}`);
+      if(JSON.stringify(id) === JSON.stringify(reviewByISBN)){
+        if(JSON.stringify(keys) === JSON.stringify("title") || JSON.stringify(keys) === JSON.stringify("author")){
+          console.log(keys, ":", values)
+          showISBNTitle = JSON.stringify(keys).concat(JSON.stringify(values))
+        }
+        if(JSON.stringify(keys) === JSON.stringify("reviews")){
+          console.log(keys, ":", values)
+          reviewsForISBN.push(keys,JSON.stringify(values))
+        }
+    }
+    });
+  });
+  return res.send(reviewByISBN + showISBNTitle + '\n' + reviewsForISBN)
 });
 
 module.exports.general = public_users;
