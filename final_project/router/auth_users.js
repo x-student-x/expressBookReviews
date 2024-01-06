d46aba97593968100d1fc4e671ae20f80dfb142f
x-student-x <148026385+x-student-x@users.noreply.github.com>
2024-01-05 22:14:26 -0600

    review add and del

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -55,10 +55,11 @@ regd_users.post("/login", (req,res) => {
 regd_users.put("/auth/review/:isbn", (req, res) => {
   //Write your code here
   //return res.status(300).json({message: "Yet to be implemented"});
- const isbn = req.params.isbn;
+    const isbn = req.params.isbn;
+    //const review = req.params.review;
     const review = req.body.review;
     const username = req.session.authorization.username;
-    console.log("add review: ", req.params, req.body, req.session);
+    //console.log("add review: ", req.params, req.body, req.session);
     if (books[isbn]) {
         let book = books[isbn];
         book.reviews[username] = review;
@@ -67,6 +68,20 @@ regd_users.put("/auth/review/:isbn", (req, res) => {
     else {
         return res.status(404).json({message: `ISBN ${isbn} not found`});
     }
+});
+
+// Delete a book review
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+  const isbn = req.params.isbn;
+  const username = req.session.authorization.username;
+  if (books[isbn]) {
+      let book = books[isbn];
+      delete book.reviews[username];
+      return res.status(200).send("Review successfully deleted");
+  }
+  else {
+      return res.status(404).json({message: `ISBN ${isbn} not found`});
+  }
 });
 
 module.exports.authenticated = regd_users;
